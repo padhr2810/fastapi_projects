aee62d7cc786f50ad5dae8d501ef395d4b922f77
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-02-26 14:53:06 +0000

    Add files via upload

--- llm_fastapi_server.py
+++ llm_fastapi_server.py
@@ -1,5 +1,6 @@
 
 import contextlib
+import json
 import os
 import time 
 
@@ -12,7 +13,7 @@ import transformers
 #############################  PREDICTION ENDPOINT:
 
 class PromptInput(BaseModel):
-    text: str
+    text: str         ##### Thus, 'text' is field name in JSON data from client.
 
 class ResponseOutput(BaseModel):
     resp: str
@@ -22,7 +23,7 @@ class LlmModel:
     def load_model(self) -> None:
         """Loads the model"""
         MODEL_NAME = "falcon-7b-instruct"
-        hw = "cuda:0"
+        #hw = "cuda:0"
         hw = "cpu" 
         model =  transformers.AutoModelForCausalLM.from_pretrained(
             MODEL_NAME , 
@@ -30,17 +31,26 @@ class LlmModel:
             torch_dtype=torch.bfloat16,
             ).to(hw)
         tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_NAME)
+        self.model = model 
+        self.tokenizer = tokenizer 
+        self.hw = hw
 
     async def predict(self, prompt: PromptInput) -> ResponseOutput:
         """Runs a prediction"""
-        if not self.model:
-            raise RuntimeError("Model is not loaded")
-        
-        prompt = prompt.replace("_", " ")
+        print(f"type of prompt = {type(prompt)}")
+        print(f"prompt = {prompt}")
+        print(f"prompt json = {prompt.json() }")
+        prompt_json = prompt.json() 
 
-        tokenized_prompt_example = tokenizer(prompt, return_tensors='pt').to("cuda:0")
-        outputs = model.generate(tokenized_prompt_example["input_ids"].to(hardware)  , max_new_tokens=150, do_sample=False, top_k=5, top_p=0.95)
-        answer = tokenizer.batch_decode( outputs, skip_special_tokens=True)
+        print(f"type of prompt_json = {type(prompt_json)}")
+        prompt_json = json.loads(prompt_json)
+        print(f"prompt_json['text'] = {prompt_json['text']}")
+        prompt = prompt_json['text']
+        prompt = prompt.replace("_", " ")
+        print(f"prompt = {prompt}")
+        tokenized_prompt_example = self.tokenizer(prompt, return_tensors='pt').to(self.hw)
+        outputs = self.model.generate(tokenized_prompt_example["input_ids"].to(self.hw)  , max_new_tokens=150, do_sample=False, top_k=5, top_p=0.95)
+        answer = self.tokenizer.batch_decode( outputs, skip_special_tokens=True)
         ## print(answer[0].rstrip() )
         response =  answer[0].rstrip()
         
