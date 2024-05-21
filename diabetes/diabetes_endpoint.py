8377609ac7cabe43e9a9ea01bced4c230b6069f3
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-05-21 12:01:14 +0100

    Add files via upload

--- diabetes/diabetes_endpoint.py
+++ diabetes/diabetes_endpoint.py
@@ -3,15 +3,23 @@
 GOOD
 http http://127.0.0.1:8000
 http http://127.0.0.1:8000/patients/TimmyOToole
+http http://127.0.0.1:8000/disease/diabetes
+http http://127.0.0.1:8000/disease/chf
 
 ERRORS
 http http://127.0.0.1:8000/patients
+http http://127.0.0.1:8000/disease/Diabetes
 
 """
 
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
+
+class DiseaseType(str, Enum):
+    DIABETES = "diabetes"
+    CHF = "chf"
 
 @app.get("/")
 async def empty_request():
@@ -32,5 +40,15 @@ hospitals = {"1": "Brigham", "2": "MGH"}
 @app.get("/hospitals")
 async def get_hospital():
     return hospitals
+
+@app.get("/disease/{disease}")
+async def get_patients(disease: DiseaseType):
+    if disease == "diabetes": 
+        px_sublist = ["diabetic_guy_1", "diabetic_guy_2"]
+    elif disease == "chf":
+        px_sublist = ["chf_guy_1", "chf_guy_2"] 
+    
+    return {disease: px_sublist}
+
 
 
