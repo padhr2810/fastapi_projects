6d46665d9ea47d06d6c3e31807551bd23ae098e1
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-05-22 12:16:32 +0100

    Add files via upload

--- relational_db/sqlite_tutorial.py
+++ relational_db/sqlite_tutorial.py
@@ -1,5 +1,25 @@
 
 
+"""
+https://docs.python.org/3/library/sqlite3.html
+
+Run this as a standalone Python module.
+
+How-to guides for further reading:
+
+        How to use placeholders to bind values in SQL queries
+
+        How to adapt custom Python types to SQLite values
+
+        How to convert SQLite values to custom Python types
+
+        How to use the connection context manager
+
+        How to create and use row factories
+
+        Explanation for in-depth background on transaction control.
+"""
+
 import sqlite3
 con = sqlite3.connect("tutorial.db")    # IMPLICITLY CREATES DB IF DOESN'T ALREADY EXIST.
 
@@ -36,6 +56,33 @@ con.commit()
 # verify that the data was inserted correctly by executing a SELECT query. 
 res = cur.execute("SELECT score FROM movie")
 print(res.fetchall() )       # fetch all rows.
+
+# insert three more rows by calling cur.executemany(...) 
+data = [
+    ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+    ("Monty Python's The Meaning of Life", 1983, 7.5),
+    ("Monty Python's Life of Brian", 1979, 8.0),
+]
+cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+con.commit()  # Remember to commit the transaction after executing INSERT.
+
+# Notice that ? placeholders are used to bind data to the query. Always use placeholders instead of string formatting to bind Python values to SQL statements, to avoid SQL injection attacks (see How to use placeholders to bind values in SQL queries for more details).
+
+
+# We can verify that the new rows were inserted by executing a SELECT query, this time iterating over the results of the query:
+for row in cur.execute("SELECT year, title FROM movie ORDER BY year"):
+    print(row)
+
+# Finally, verify that the database has been written to disk by calling con.close() to close the existing connection, opening a new one, creating a new cursor, then querying the database:
+con.close()
+
+new_con = sqlite3.connect("tutorial.db")
+new_cur = new_con.cursor()
+res = new_cur.execute("SELECT title, year FROM movie ORDER BY score DESC")
+title, year = res.fetchone()
+
+print(f'The highest scoring Monty Python movie is {title!r}, released in {year}')
+new_con.close()
 
 
 
